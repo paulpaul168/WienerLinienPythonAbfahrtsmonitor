1dc0b5dd5d7416569375f9d78de90eeeae1ef1a4
paulpaul168 <paulh@outlook.at>
2020-03-22 20:40:10 +0000

    tkinter with background and fullscreen

--- __main__.py
+++ __main__.py
@@ -1,5 +1,6 @@
-import tkinter, json, urllib.request, threading
+import tkinter, json, urllib.request
 
+TIME_RESTART = 20000 #in ms
 
 def getResponse(url):
     operUrl = urllib.request.urlopen(url)
@@ -11,15 +12,19 @@ def getResponse(url):
 
 
 def updateData():
-	threading.Timer(30, updateData).start()
 	nussdorf = getResponse('https://www.wienerlinien.at/ogd_realtime/monitor?rbl=90')
 	j_obj_nussdorf = json.loads(nussdorf.decode())
 	D_1 = j_obj_nussdorf['data']['monitors'][0]['lines'][0]['departures']['departure'][0]['departureTime']['countdown']
 	D_2 = j_obj_nussdorf['data']['monitors'][0]['lines'][0]['departures']['departure'][1]['departureTime']['countdown']
 	print("D1: "+str(D_1)+"min ; D2: "+str(D_2)+"min")
+	monitor.after(TIME_RESTART, updateData)
 
-#top = tkinter.Tk()
-#top.mainloop()
 
 if __name__ == "__main__":
-	updateData()
+	monitor = tkinter.Tk()
+	monitor.configure(background='black')
+	monitor.attributes("-fullscreen",True)
+	width = monitor.winfo_screenwidth()
+	height = monitor.winfo_screenheight()
+	monitor.after(TIME_RESTART, updateData)
+	monitor.mainloop()
