a64aec5da5bcfde0dd09a25d6dfda4aaaff7a8d4
paulpaul168 <35666071+paulpaul168@users.noreply.github.com>
2020-03-30 17:33:03 +0200

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -54,6 +54,7 @@ if __name__ == "__main__":
 	clock_label.place(x=width*0.35,y=height*0.05)
 	loading_label = Label(monitor,text="Updating...", fg="White", font=myFont, bg="Black")
 	loading_label.place(x=width*0.25,y=height*0.35)
+	time.sleep(0.5)
 	os.system("cd ~/WienerLinienPythonAbfahrtsmonitor/ && git pull")
 	loading_label.configure(text="Loading...")
 	monitor.after(TIME_RESTART, updateData)
